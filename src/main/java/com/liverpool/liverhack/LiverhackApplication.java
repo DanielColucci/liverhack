50140bd9b7ab422237ded26fa5ba000b536035d9
Daniel Colucci <142242668+DanielColucci@users.noreply.github.com>
2026-09-24 10:12:23 -0600

    Add files via upload

--- src/main/java/com/liverpool/liverhack/LiverhackApplication.java
+++ src/main/java/com/liverpool/liverhack/LiverhackApplication.java
@@ -2,8 +2,10 @@ package com.liverpool.liverhack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class LiverhackApplication {
 
 	public static void main(String[] args) {
