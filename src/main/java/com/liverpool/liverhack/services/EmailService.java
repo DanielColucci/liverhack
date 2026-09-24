de2269e6419f8ab8b0b6c68cd2aa0eba391d69b4
Daniel Colucci <142242668+DanielColucci@users.noreply.github.com>
2026-09-24 09:50:46 -0600

    Add files via upload

--- src/main/java/com/liverpool/liverhack/services/EmailService.java
+++ src/main/java/com/liverpool/liverhack/services/EmailService.java
@@ -31,7 +31,7 @@ public class EmailService {
             message.setSubject("Liverpool Talento - Entrevista con Hiring Manager");
             String fechaAmigable = fechaHora.replace("T", " a las ");
             // Cambiado a 8082
-            String texto = "¡Felicidades " + nombre + "!\n\nHas llegado a la etapa final de nuestro proceso de selección.\nSe ha programado tu Entrevista Final con el Hiring Manager para la siguiente fecha:\n👉 " + fechaAmigable + " hrs.\n\nInicia sesión en tu Portal de Talento para confirmar tu asistencia:\nhttp://localhost:8082/login\n\nAtentamente,\nEquipo de Atracción de Talento Liverpool";
+            String texto = "¡Felicidades " + nombre + "!\n\nHas llegado a la etapa final de nuestro proceso de selección.\nSe ha programado tu Entrevista Final con el Hiring Manager para la siguiente fecha:\n👉 " + fechaAmigable + " hrs.\n\nInicia sesión en tu Portal de Talento para confirmar tu asistencia:\nliverhack-production.up.railway.app\n\nAtentamente,\nEquipo de Atracción de Talento Liverpool";
             message.setText(texto);
             mailSender.send(message);
         } catch (Exception e) { System.out.println("Error enviando correo HM: " + e.getMessage()); }
@@ -43,7 +43,7 @@ public class EmailService {
             message.setTo(destinatario);
             message.setSubject("Liverpool Talento - ¡Tenemos una Oferta para ti!");
             // Cambiado a 8082
-            String texto = "¡Felicidades " + nombre + "!\n\nTenemos excelentes noticias. Entra a tu Portal de Talento para revisar tu Carta Oferta y las condiciones laborales propuestas.\n\nhttp://localhost:8082/login\n\nAtentamente,\nEquipo de Atracción de Talento Liverpool";
+            String texto = "¡Felicidades " + nombre + "!\n\nTenemos excelentes noticias. Entra a tu Portal de Talento para revisar tu Carta Oferta y las condiciones laborales propuestas.\n\nliverhack-production.up.railway.app\n\nAtentamente,\nEquipo de Atracción de Talento Liverpool";
             message.setText(texto);
             mailSender.send(message);
         } catch (Exception e) { System.out.println("Error enviando correo Oferta: " + e.getMessage()); }
