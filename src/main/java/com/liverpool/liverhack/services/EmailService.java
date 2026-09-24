cdffd6ba8d4bd0e04ce7e2aa7e09b1a29aa5ebd4
Daniel Colucci <142242668+DanielColucci@users.noreply.github.com>
2026-09-24 10:11:54 -0600

    Add files via upload

--- src/main/java/com/liverpool/liverhack/services/EmailService.java
+++ src/main/java/com/liverpool/liverhack/services/EmailService.java
@@ -3,6 +3,7 @@ package com.liverpool.liverhack.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,44 +12,45 @@ public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
+    @Async
     public void enviarCorreoEntrevista(String destinatario, String nombre, String fechaHora) {
         try {
             SimpleMailMessage message = new SimpleMailMessage();
             message.setTo(destinatario);
             message.setSubject("Liverpool Talento - Entrevista por Competencias");
             String fechaAmigable = fechaHora.replace("T", " a las ");
-            // Cambiado a 8082
-            String texto = "¡Hola " + nombre + "!\n\nHas avanzado en nuestro proceso de selección.\nAtracción de Talento ha programado tu Entrevista para la siguiente fecha:\n👉 " + fechaAmigable + " hrs.\n\nInicia sesión en tu Portal de Talento para confirmar tu asistencia:\nhttp://localhost:8082/login\n\nAtentamente,\nEquipo de Atracción de Talento Liverpool";
+            String texto = "¡Hola " + nombre + "!\n\nHas avanzado en nuestro proceso de selección.\nAtracción de Talento ha programado tu Entrevista para la siguiente fecha:\n👉 " + fechaAmigable + " hrs.\n\nInicia sesión en tu Portal de Talento para confirmar tu asistencia:\nliverhack-production.up.railway.app\n\nAtentamente,\nEquipo de Atracción de Talento Liverpool";
             message.setText(texto);
             mailSender.send(message);
         } catch (Exception e) { System.out.println("Error enviando correo: " + e.getMessage()); }
     }
 
+    @Async
     public void enviarCorreoEntrevistaHM(String destinatario, String nombre, String fechaHora) {
         try {
             SimpleMailMessage message = new SimpleMailMessage();
             message.setTo(destinatario);
             message.setSubject("Liverpool Talento - Entrevista con Hiring Manager");
             String fechaAmigable = fechaHora.replace("T", " a las ");
-            // Cambiado a 8082
             String texto = "¡Felicidades " + nombre + "!\n\nHas llegado a la etapa final de nuestro proceso de selección.\nSe ha programado tu Entrevista Final con el Hiring Manager para la siguiente fecha:\n👉 " + fechaAmigable + " hrs.\n\nInicia sesión en tu Portal de Talento para confirmar tu asistencia:\nliverhack-production.up.railway.app\n\nAtentamente,\nEquipo de Atracción de Talento Liverpool";
             message.setText(texto);
             mailSender.send(message);
         } catch (Exception e) { System.out.println("Error enviando correo HM: " + e.getMessage()); }
     }
 
+    @Async
     public void enviarCorreoOferta(String destinatario, String nombre) {
         try {
             SimpleMailMessage message = new SimpleMailMessage();
             message.setTo(destinatario);
             message.setSubject("Liverpool Talento - ¡Tenemos una Oferta para ti!");
-            // Cambiado a 8082
             String texto = "¡Felicidades " + nombre + "!\n\nTenemos excelentes noticias. Entra a tu Portal de Talento para revisar tu Carta Oferta y las condiciones laborales propuestas.\n\nliverhack-production.up.railway.app\n\nAtentamente,\nEquipo de Atracción de Talento Liverpool";
             message.setText(texto);
             mailSender.send(message);
         } catch (Exception e) { System.out.println("Error enviando correo Oferta: " + e.getMessage()); }
     }
 
+    @Async
     public void enviarCorreoBienvenida(String destinatario, String nombre, String fechaIngreso) {
         try {
             SimpleMailMessage message = new SimpleMailMessage();
